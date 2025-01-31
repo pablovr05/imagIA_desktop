61c22041e4a2607a090e707a638c4f217e682ec2
Jonathan Rueda <jonaneiro09@gmail.com>
2025-01-31 19:20:51 +0100

    cambiar cosas
    Tengo que juntarlo con el codigo que hay en mi pc

--- desktop/lib/main_view.dart
+++ desktop/lib/main_view.dart
@@ -1,112 +1,115 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
 
-class MainView extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'api_service.dart';
+
+class MainView extends StatefulWidget {
   const MainView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        double width = constraints.maxWidth;
-        bool isSmallScreen = width < 800;
+  _MainViewState createState() => _MainViewState();
+}
 
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          home: Scaffold(
-            backgroundColor: Colors.white,
-            body: Padding(
-              padding: const EdgeInsets.only(top: 40.0),
-              child: Row(
+class _MainViewState extends State<MainView> {
+  List<dynamic> users = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUsers();
+  }
+
+  Future<void> fetchUsers() async {
+    try {
+      String baseUrl = await _loadURL();
+      if (baseUrl.isEmpty) {
+        print('Error: No se pudo cargar la URL del servidor.');
+        return;
+      }
+
+      ApiService apiService = ApiService(baseUrl: baseUrl);
+      final fetchedUsers = await apiService.getAllUsers(context);
+      setState(() {
+        users = (fetchedUsers['data']['users'] as List<dynamic>?)?.map((user) {
+              return {
+                "phone": user["phone"] ?? "N/A",
+                "nickname": user["nickname"] ?? "N/A",
+                "email": user["email"] ?? "N/A",
+                "type_id": user["type_id"] ?? "FREE",
+                "created_at": user["created_at"] ?? "yyyy-mm-dd HH:MM:SS",
+                "updated_at": user["updated_at"] ?? "yyyy-mm-dd HH:MM:SS",
+              };
+            }).toList() ??
+            [];
+      });
+    } catch (e) {
+      print('Error fetching users: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.only(top: 40.0),
+          child: Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
                 children: [
-                  if (width >= 800)
-                    Expanded(flex: 2, child: Container(color: Colors.white)),
-                  Expanded(
-                    flex: width < 800 ? 9 : 10,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(16.0),
-                      ),
-                      margin: const EdgeInsets.symmetric(horizontal: 16.0),
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.symmetric(vertical: 10.0),
-                            decoration: const BoxDecoration(
-                              border: Border(
-                                bottom: BorderSide(color: Colors.white70),
-                              ),
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                const Text('Username',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold)),
-                                const Text('Phone',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold)),
-                                if (!isSmallScreen)
-                                  const Text('Created',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold)),
-                                if (!isSmallScreen)
-                                  const Text('Updated',
-                                      style: TextStyle(
-                                          color: Colors.white,
-                                          fontWeight: FontWeight.bold)),
-                                const Text('Type',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold)),
-                              ],
-                            ),
-                          ),
-                          Expanded(
-                            child: SingleChildScrollView(
-                              child: Column(
-                                children: List.generate(
-                                  10,
-                                  (index) => CustomListItem(
-                                    index: index,
-                                    isSmallScreen: isSmallScreen,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                  const Text(
+                    'Lista de Usuarios',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
                   ),
-                  if (width >= 750)
-                    Expanded(flex: 2, child: Container(color: Colors.white)),
+                  Expanded(
+                    child: users.isEmpty
+                        ? const Center(child: CircularProgressIndicator())
+                        : ListView.builder(
+                            itemCount: users.length,
+                            itemBuilder: (context, index) {
+                              return CustomListItem(user: users[index]);
+                            },
+                          ),
+                  ),
                 ],
               ),
             ),
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
 
 class CustomListItem extends StatefulWidget {
-  final int index;
-  final bool isSmallScreen;
-  const CustomListItem(
-      {required this.index, required this.isSmallScreen, super.key});
+  final dynamic user;
+  const CustomListItem({required this.user, super.key});
 
   @override
   _CustomListItemState createState() => _CustomListItemState();
 }
 
 class _CustomListItemState extends State<CustomListItem> {
-  String selectedType = 'FREE';
+  late String selectedType;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedType =
+        widget.user['type_id'] ?? 'FREE'; // Asegúrate de usar 'type_id'
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -127,13 +130,14 @@ class _CustomListItemState extends State<CustomListItem> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Text('User ${widget.index}',
+          Text(widget.user['nickname'] ?? 'N/A',
               style: const TextStyle(color: Colors.white)),
-          const Text('123456789', style: TextStyle(color: Colors.white)),
-          if (!widget.isSmallScreen)
-            const Text('2023-01-01', style: TextStyle(color: Colors.white)),
-          if (!widget.isSmallScreen)
-            const Text('2023-01-02', style: TextStyle(color: Colors.white)),
+          Text(widget.user['phone'] ?? 'N/A',
+              style: const TextStyle(color: Colors.white)),
+          Text(widget.user['created_at'] ?? 'N/A',
+              style: const TextStyle(color: Colors.white)),
+          Text(widget.user['updated_at'] ?? 'N/A',
+              style: const TextStyle(color: Colors.white)),
           DropdownButton<String>(
             dropdownColor: Colors.black,
             value: selectedType,
@@ -145,16 +149,35 @@ class _CustomListItemState extends State<CustomListItem> {
                     ))
                 .toList(),
             onChanged: (String? newValue) {
-              if (newValue != null) {
-                setState(() {
-                  selectedType = newValue;
-                });
-                print('Se ha cambiado a $newValue');
-              }
+              setState(() {
+                selectedType = newValue!;
+              });
+              print('Se ha cambiado a $newValue');
             },
           ),
         ],
       ),
     );
   }
+}
+
+// Cargar datos del archivo JSON si existe
+Future<dynamic> _loadURL() async {
+  try {
+    const path = './data/';
+    final file = File('$path/data.json');
+
+    if (file.existsSync()) {
+      final String content = await file.readAsString();
+      final Map<String, dynamic> data = jsonDecode(content);
+
+      if (data.containsKey('ServerKey')) {
+        print(data['serverKey']);
+        return data['ServerKey'];
+      }
+    }
+  } catch (e) {
+    print('Error al cargar los datos: $e');
+  }
+  return '';
 }
